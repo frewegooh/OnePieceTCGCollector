c9a364f99eb2192f46d96a809c2c78a352dff70b
frewegooh <frewegooh@gmail.com>
2024-12-16 21:19:56 -0500

    Updated config.js to use separate URLs for API and static assets

--- src/config.js
+++ src/config.js
@@ -2,8 +2,13 @@ const API_URL = process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
     : 'https://card-tracker-962993019749.us-central1.run.app';
 
+const STATIC_URL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : 'https://card-tracker-c0d1f.web.app';
+
 export const getImageUrl = (imageUrl) => {
-    return `${API_URL}/download-image?imageUrl=${encodeURIComponent(imageUrl)}`;
+    const imageName = imageUrl.split('/').pop().replace('_200w.jpg', '_400w.jpg');
+    return `${STATIC_URL}/images/${imageName}`;
 };
 
 export default API_URL;
